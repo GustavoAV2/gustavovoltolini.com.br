5276e1d5b808805a7d6771c1a3df23346cc1e09a
Gustavo Voltolini <gustavoant.voltolini@gmail.com>
2023-10-24 15:03:49 -0300

    adding spacing and mobile styling

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,46 +1,47 @@
 <script>
-export default {};
+export default {
+  methods: {
+    isMobile() {
+      if (screen.width <= 760) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
 </script>
 
 <template>
-  <nav class="bg-white border-gray-200 text-xl">
-    <div
-      class="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl p-4"
-    >
+  <nav class="bg-white border-gray-200 text-lg md:text-xl">
+    <div class="flex justify-center items-center mx-auto max-w-screen-xl p-4">
       <div
         id="mega-menu-full"
-        class="items-center justify-center font-medium hidden w-full md:flex md:w-auto md:order-1 sm:flex sm:w-auto"
+        class="items-center justify-center font-medium w-full flex md:w-auto order-1 sm:flex sm:w-auto"
       >
         <a
+          v-if="!isMobile()"
           href="/"
-          class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
+          class="block py-2 pl-3 p-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
           aria-current="page"
           >Inicio</a
         >
 
         <a
           href="/profile"
-          class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
+          class="block py-2 pl-3 p-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
           aria-current="page"
           >Resumo</a
         >
 
-        <!-- <li>
-            <a
-              href="/projects"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
-              >Projetos</a
-            >
-          </li> -->
-
         <a
           href="/hackathons"
-          class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
+          class="block py-2 pl-3 p-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
           >Hackathons</a
         >
         <a
           href="/about"
-          class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
+          class="block py-2 pl-3 p-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-5"
           >Contato</a
         >
       </div>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,14 +1,16 @@
 <script>
-import HeaderComponent from '../components/shared/Header.vue'
-import Profile from '../components/Profile.vue'
+import HeaderComponent from "../components/shared/Header.vue";
+import Profile from "../components/Profile.vue";
 export default {
-  components: { HeaderComponent, Profile }
-}
+  components: { HeaderComponent, Profile },
+};
 </script>
 
 <template>
   <HeaderComponent></HeaderComponent>
-  <div class="flex-col justify-center w-auto inline-block p-10">
+  <div
+    class="flex-col justify-center w-auto inline-block p-1 pb-4 md:pr-96 md:pl-96"
+  >
     <Profile></Profile>
   </div>
 </template>
