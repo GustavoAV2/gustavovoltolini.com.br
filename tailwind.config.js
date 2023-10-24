cb2d74a245eb522f839d7294e5c7761ded8332fd
Gustavo Voltolini <gustavoant.voltolini@gmail.com>
2023-10-24 10:17:45 -0300

    Trying fixing tailwind confiog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
