078b2b524f1bc0e7fe0fb2493d9c9a572ccb3ce4
Gustavo Antunes Voltolini <gustavoant.voltolini@gmail.com>
2023-10-25 18:52:32 -0300

    Creating NotFound and Hackathon detail route

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ProjectView from '../views/ProjectView.vue'
-import ProfileView from '../views/ProfileView.vue'
-import HackathonView from '../views/HackathonView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ProjectView from "../views/ProjectView.vue";
+import ProfileView from "../views/ProfileView.vue";
+import HackathonsView from "../views/HackathonView.vue";
+import HackathonSambaMeetsView from "../views/hackathons/HackathonSambaMeetsView.vue";
+import PageNotFoundView from "../views/PageNotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/profile',
-      name: 'profile',
-      component: ProfileView
+      path: "/profile",
+      name: "profile",
+      component: ProfileView,
     },
     {
-      path: '/projects',
-      name: 'projects',
-      component: ProjectView
+      path: "/projects",
+      name: "projects",
+      component: ProjectView,
     },
     {
-      path: '/hackathons',
-      name: 'hackathons',
-      component: HackathonView
+      path: "/hackathons",
+      name: "hackathons",
+      component: HackathonsView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: AboutView
-    }
-  ]
-})
+      path: "/hackathon/sambameets",
+      name: "hackathon",
+      component: HackathonSambaMeetsView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView,
+    },
+    {
+      path: "/:catchAll(.*)", // Unrecognized path automatically matches 404
+      redirect: "/404",
+    },
+    {
+      path: "/404",
+      component: PageNotFoundView,
+    },
+  ],
+});
 
-export default router
+export default router;
