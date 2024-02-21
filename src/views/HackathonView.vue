136f6c58e63e9110c3f19d6a94b27063a2289ffd
Gustavo Voltolini <gustavoant.voltolini@gmail.com>
2024-02-21 12:22:34 -0300

    Update HackathonView.vue

--- src/views/HackathonView.vue
+++ src/views/HackathonView.vue
@@ -111,6 +111,7 @@ export default {
       el_date.classList.add("text-gray-900");
       el_rank.classList.remove("font-medium");
       el_rank.classList.remove("text-gray-900");
+      this.tooltipVisible();
     },
     filterByRank() {
       let el_date = document.getElementById("menu-data-filter");
@@ -120,6 +121,7 @@ export default {
       el_rank.classList.add("text-gray-900");
       el_date.classList.remove("font-medium");
       el_date.classList.remove("text-gray-900");
+      this.tooltipVisible();
     },
   },
 };
