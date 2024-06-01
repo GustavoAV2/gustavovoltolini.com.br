58720c95082491e7beafa6f36f2c7e7e04d70755
Gustavo Antunes Voltolini <gustavoant.voltolini@gmail.com>
2024-06-01 00:55:18 -0300

    Unused code deleted

--- server/app.js
+++ server/app.js
@@ -1,26 +1,16 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import { getVisitors, postVisitor } from "./visitorsService.js";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("api/visitor", async function (req, res) {
-  try {
-    const visitors = await getVisitors();
-    res.send(visitors);
-  } catch (error) {
-    res.status(500).send({ error: "Internal server error" });
-  }
-});
-
 app.post("api/visitor", async function (req, res) {
   try {
     const newVisitor = req.body;
-    const visitors = await postVisitor(newVisitor);
+    const visitors = await sendVisitor(newVisitor);
     res.send(visitors);
   } catch (error) {
     res.status(500).send({ error: "Internal server error" });
